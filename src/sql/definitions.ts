04eb29ebafce3a9c7a740663689d477accc2a80d
Harry Park <harry@harrypark.io>
2018-11-28 14:39:31 +1100

    feat: sql definitions

---
+++ src/sql/definitions.ts
@@ -0,0 +1,22 @@
+export interface TypeDefinition {
+  name: string;
+  type: string;
+}
+
+export interface ColumnDefinition {
+  name: string;
+  nullable: boolean;
+  type: TypeDefinition;
+}
+
+export interface TableDefinition {
+  name: string;
+  schema?: string;
+  columns: ColumnDefinition[];
+}
+
+export interface EnumDefinition {
+  name: string;
+  schema?: string;
+  values: any[];
+}
